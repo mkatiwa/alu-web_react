bd67cf6e38a9799ab77a3920db915c09b2023b2b
mkatiwa <m.katiwa@alustudent.com>
2025-01-17 12:18:25 +0300

    react intro

---
+++ react-intro/src/task_0/dashboards/App.js
@@ -0,0 +1,15 @@
+import logo from './assets/Holberton-logo.png';
+import "./App.css";
+
+function App () {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} alt="Holberton logo" />
+                <h1>School Dashboard</h1>
+        </div>
+            </div>
+    );
+}
+
+export default App;
